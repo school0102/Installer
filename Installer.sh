f0b3abc894af05e70b63eb2cc25fc3fbf4eeeffe
school0102 <hugo.schneider@escola.pr.gov.br>
2026-05-27 11:44:28 -0300

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1,16 +1,7 @@
 # ============================
-# Inicia com extensão
+# Inicia com extensão (SINGLE LINE METHOD)
 # ============================
 
-echo "🚀 Iniciando Chrome com a extensão..."
+echo "🚀 Iniciando Chrome em modo debug..."
 
-# O uso da barra invertida '\' no final de cada linha é OBRIGATÓRIO 
-# para o Linux entender que é o mesmo comando continuado embaixo.
-"$CHROME_BIN" \
-    --user-data-dir="$USER_DATA_DIR" \
-    --load-extension="$EXT_PATH" \
-    --restore-last-session \
-    --no-first-run \
-    >/dev/null 2>&1 &
-
-echo "✅ Extensão carregada com sucesso!"
+"$CHROME_BIN" --user-data-dir="$USER_DATA_DIR" --load-extension="$EXT_PATH" --no-first-run
