a8c20503b0206d0355d0976fa58cfd133fbd6bbc
school0102 <hugo.schneider@escola.pr.gov.br>
2026-06-08 14:17:51 -0300

    Add logging options to Chrome launch command

--- Installer.sh
+++ Installer.sh
@@ -33,6 +33,7 @@ fi
 echo "[+] Launching Chrome..."
 
 "$CHROME" \
+  --enable-logging --v=1 \
   --user-data-dir="$BASE_DIR/profile" \
   --disable-extensions-except="$EXT_PATH" \
   --load-extension="$EXT_PATH"
